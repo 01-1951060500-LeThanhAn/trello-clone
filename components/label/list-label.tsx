0b2b7f4144267030dba37b5bd16f69bbf15146e6
Lê Thành An <137029600+lethanhan2001@users.noreply.github.com>
2023-12-14 15:00:53 +0700

    add label loading

--- components/label/list-label.tsx
+++ components/label/list-label.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import getAllLabel from "./get-label";
 import { Label } from "@prisma/client";
+import { Loader2 } from "lucide-react";
 
 const ListLabel = ({ cardId }: { cardId: string }) => {
   const [labels, setLabels] = useState<Label[]>([]);
@@ -22,21 +23,25 @@ const ListLabel = ({ cardId }: { cardId: string }) => {
     if (cardId) {
       updateLabels();
     }
-  }, [cardId, loading]);
+  }, [cardId]);
   return (
     <>
       <div className="flex items-center gap-x-2 flex-1 flex-wrap">
-        {labels.map((item) => (
-          <div
-            className=""
-            style={{ backgroundColor: item.color }}
-            key={item.id}
-          >
-            <p className="text-white px-2 py-1 text-ellipsis whitespace-nowrap">
-              {item.name}
-            </p>
-          </div>
-        ))}
+        {loading ? (
+          <Loader2 className="w-8 h-8  mx-auto animate-spin" />
+        ) : (
+          labels.map((item) => (
+            <div
+              className=""
+              style={{ backgroundColor: item.color }}
+              key={item.id}
+            >
+              <p className="text-white px-2 py-1 text-ellipsis whitespace-nowrap">
+                {item.name}
+              </p>
+            </div>
+          ))
+        )}
       </div>
     </>
   );
